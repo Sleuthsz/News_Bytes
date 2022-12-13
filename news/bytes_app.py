04996f00cfc671088ece4640746911f827c9f07a
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-12-12 21:39:52 -0800

    WIP finetune text to send to OpenAI

--- news/bytes_app.py
+++ news/bytes_app.py
@@ -1,11 +1,21 @@
+import os
+import openai
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 options = Options()
 options.headless = True
 
-driver = webdriver.Chrome(options=options)
+homedir = os.path.expanduser("~")
+webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
+
+driver = webdriver.Chrome(options=options, service=webdriver_service)
 driver.get("https://www.bbc.com")
 driver.implicitly_wait(1)
 
@@ -21,4 +31,20 @@ def get_headlines():
 
 
 if __name__ == "__main__":
-    print(get_headlines())
+    # print(get_headlines()
+    links = get_headlines()
+    print(links[1][1])
+    print("==================")
+    driver.get(links[1][1])
+    element = driver.find_element(By.TAG_NAME, "article")
+    print(element.text)
+    prompt = f"Summarize this: {element.text}"
+    # print(prompt)
+    # response = openai.Completion.create(
+    #     model="text-ada-001",
+    #     prompt=prompt,
+    #     temperature=0.6,
+    # )
+    # print("==================")
+    #
+    # print(response)
