ca2311ce01040370556ac59ed974e523dfceb5a2
Natalija Germek <n.germek@icloud.com>
2022-12-15 22:23:40 -0800

    complete headline scrape and headline link scrape, started category scrape.

--- news/nbc.py
+++ news/nbc.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-nbc_key = os.getenv("NBC_KEY")
+# nbc_key = os.getenv("NBC_KEY")
 # GET https://newsapi.org/v2/top-headlines?country=us&apiKey=nbc_key
 
 options = Options()
@@ -18,23 +18,21 @@ webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
 
 nbc_driver = webdriver.Chrome(options=options, service=webdriver_service)
 
-nbc_driver.get("https://www.nbcnews.com/")
+nbc_driver.get("https://www.nbcnews.com")
 nbc_driver.implicitly_wait(1)
-
-print(nbc_driver)
 
 
 def get_headlines():
-    link = nbc_driver.find_elements(By.CLASS_NAME, "tease-card__headline")
+    link = nbc_driver.find_elements(By.CLASS_NAME, "related-content__headline-link")
 
     headlines_links = []
-    for i in range(10):
+    for i in range(len(link)):
         headlines_links.append((link[i].text, link[i].get_attribute('href')))
+        # print(link[i].get_attribute('href'))
 
     return headlines_links
 
 
-# TODO: get links to populate correctly
 def get_article_text(link):
     nbc_driver.get(link)
     article = nbc_driver.find_element(By.TAG_NAME, "article")
@@ -45,11 +43,50 @@ def get_article_text(link):
     return text
 
 
+def get_business():
+    # business_link = https://www.nbcnews.com/business
+    pass
+
+
+def get_world():
+    # world_link = https://www.nbcnews.com/world
+    pass
+
+
+def get_tech():
+    # tech_link = https://www.nbcnews.com/tech-media
+    pass
+
+
+def get_sports():
+    # sports_link = https://www.nbcsports.com/?cid=eref:nbcnews:text
+    pass
+
+
+# too broad - refactor needed
+# def get_categories():
+#     categories_link = nbc_driver.find_elements(By.CLASS_NAME, "shortcuts-list-item")
+#     category = nbc_driver.find_element(By.TAG_NAME, "li")
+#     categories = []
+#     for i in range(len(categories_link)):
+#         categories.append((categories_link[i].text, categories_link[i].get_attribute('li')))
+#         print(categories_link[i].get_attribute('li'))
+#
+#     return get_categories()
+
+# ********* README.md update - created nbc.py for webscrape of nbc website, updated README.md - December 14, 2022
+# ********* README.md update - finalized headline and link scrape, started categories scrape, updated README.md. -
+# December 15, 2022
+
 if __name__ == "__main__":
     headlines = get_headlines()
     print(headlines)
-    # links = get_article_text(links)
-    # print(links)
+    links = get_article_text(headlines[1][1])
+    print(links)
+    # article_test = get_article_text()
+    # print(article_test)
+    # categories = get_categories()
+    # print()
 
     # ------------------unused until able to get rest working-------------
     # article_text = get_article_text(links[1][1])
@@ -57,8 +94,6 @@ if __name__ == "__main__":
     # print(summary)
     # print(article_text)
     # get_article_text()
-
-# ********* README.md update - created nbc.py for webscrape of nbc website, updated README.md - December 14, 2022
 
 
 # --------
@@ -85,4 +120,3 @@ if __name__ == "__main__":
 #     return response["choices"][0]["text"]
 #
 #
-
