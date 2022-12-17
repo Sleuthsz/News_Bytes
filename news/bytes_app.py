d6310c01325feee98fffed2fdb24890c5fcdb117
Daniel Brott <danbrott42@gmail.com>
2022-12-17 18:22:48 +0000

    updated menus and nested BBC in a news class

--- news/bytes_app.py
+++ news/bytes_app.py
@@ -6,61 +6,61 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-options = Options()
-options.headless = True
+class News:
+    def __init__(self):
+        self.title = ""
+        self.env = load_dotenv()
+        self.api_key= os.getenv("OPENAI_API_KEY")
+        self.options = Options()
+        self.homedir = os.path.expanduser("~")
+        self.webdriver_service = Service(f"{self.homedir}/chromedriver/stable/chromedriver")
+        self.driver = webdriver.Chrome(options=self.options, service=self.webdriver_service)
+        self.ai_model = {
+            'ada': "text-ada-001",
+            'babbage': "text-babbage-001",
+            'curie': "text-curie-001",
+            'davinci': "text-davinci-001",
+        }
 
-homedir = os.path.expanduser("~")
-webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
-
-driver = webdriver.Chrome(options=options, service=webdriver_service)
-driver.get("https://www.bbc.com")
-driver.implicitly_wait(1)
-
-openai_model = "text-ada-001"
-# openai_model = "text-babbage-001"
-# openai_model = "text-curie-001"
-# openai_model = "text-davinci-003"
-
-
-def get_headlines():
-    link = driver.find_elements(By.CLASS_NAME, "media__link")
-
-    headlines_links = []
-    for i in range(10):
-        headlines_links.append((link[i].text, link[i].get_attribute('href')))
-
-    return headlines_links
-
-
-def get_article_text(link):
-    driver.get(link)
-    article = driver.find_element(By.TAG_NAME, "article")
-    elements = article.find_elements(By.XPATH, "//div[@data-component='text-block']")
-    text = ""
-    for element in elements:
-        text += element.text
-    return text
+    @staticmethod
+    def get_summary(text):
+        prompt = f"{text}\n\nTl;dr"
+        response = openai.Completion.create(
+            model=self.ai_model['ada'],
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=140,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=1
+        )
+        return response["choices"][0]["text"]
 
 
-def get_summary(text):
-    prompt = f"{text}\n\nTl;dr"
-    response = openai.Completion.create(
-        model=openai_model,
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=140,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=1
-    )
-    return response["choices"][0]["text"]
+class BBC(News):
+    def __init__(self):
+        super().__init__()
+        self.driver = driver.get("https://www.bbc.com")
+        self.driver.implicitly_wait(1)
+
+    def get_headlines(self):
+        link = self.driver.find_elements(By.CLASS_NAME, "media__link")
+        headlines_links = []
+        for i in range(10):
+            headlines_links.append((link[i].text, link[i].get_attribute('href')))
+        return headlines_links
+
+    def get_article_text(self, link):
+        self.driver.get(link)
+        article = self.driver.find_element(By.TAG_NAME, "article")
+        elements = article.find_elements(By.XPATH, "//div[@data-component='text-block']")
+        text = ""
+        for element in elements:
+            text += element.text
+        return text
 
 
 if __name__ == "__main__":
-    links = get_headlines()
-    article_text = get_article_text(links[0][1])
-    summary = get_summary(article_text)
-    print(summary)
+    bbc_news = BBC()
+    print(bbc_news.get_headlines())
--- news/menu.py
+++ news/menu.py
@@ -7,7 +7,9 @@ from rich.progress import Progress, BarColumn, TextColumn
 from rich.progress import track
 from rich.theme import Theme
 import time
+from rich import inspect
 from rich.columns import Columns
+from rich.table import Column
 import pyfiglet
 from rich import box
 
@@ -22,23 +24,24 @@ class Menu:
     def ascii_art(self):
         self.console.print(pyfiglet.figlet_format("WELCOME TO NEWS BYTES", font="slant", width=200), justify="center")
 
-    # @staticmethod
-    def progress_bar():
-        with Progress() as progress:
-            task = progress.add_task("Get headlines", total=10)
-            for index, headline in enumerate(get_headlines()):
-                progress.console.print(f"Populating Headlines: {index}")
-                time.sleep(0.2)
-            while not progress.finished:
-                progress.update(task, advance=0.9)
+    @staticmethod
+    def progress_bar(lst:list):
+        text_column = TextColumn("{task.description}", table_column=Column(ratio=1))
+        bar_column = BarColumn(bar_width=None, table_column=Column(ratio=2))
+        progress = Progress(text_column, bar_column, expand=True)
+        with progress:
+            for _ in progress.track(lst):
+                time.sleep(0.1)
 
-    def headlines_table(self):
-        table = Table(show_lines=True, row_styles=["dim", ""], title_justify="center", box=box.HEAVY_EDGE, highlight=True)
 
-        table.add_column("Headlines", justify="center", style="light_steel_blue", header_style="light_sky_blue1")
+    def make_headlines_table(self, headlines:list[str] = get_headlines()):
+        table = Table(show_lines=True, row_styles=["cyan", "magenta"], title_justify="center", box=box.HEAVY_EDGE, highlight=True)
 
-        for index, headline in enumerate(get_headlines()):
-            table.add_row(headline[0])
+        table.add_column("#", justify="center")
+        table.add_column("[i]Headlines", justify="center", style="light_steel_blue", header_style="light_sky_blue1")
+
+        for index, headline in enumerate(headlines):
+            table.add_row(str(index + 1),headline[0])
         self.console.print(table, justify="center")
 
     def categories_panel(self):
@@ -50,7 +53,7 @@ class Menu:
         self.console.print(Panel('Enter a [red]category [white]if you wish to see more headlines,\n otherwise enter in a headline number in order to get a summary \n with a link to the article. If you wish to quit, enter (q)uit'), justify="center", style='Bold')
 
     def get_user_input(self):
-        news = ['news', 'business', 'world news', 'tech', 'sports']
+        news = ['business', 'world news', 'tech', 'sports']
         user_input = input('> ')
         while user_input.lower() != 'q' or user_input.lower() != 'quit':
             if user_input in self.get_headlines().lower():
@@ -61,7 +64,7 @@ class Menu:
                 pass
             elif user_input.lower() == 'r' or user_input.lower() == 'return':
                 #TODO Returns user to a previous menu or state
-                pass
+                continue
 
     def display_category_news(self, category:str, headlines: list[str] = get_headlines()):
         table = Table(show_lines=True, row_styles=["cyan", "magenta"], title_justify="center", box=box.HEAVY_EDGE, highlight=True)
@@ -75,12 +78,23 @@ class Menu:
         self.console.print(table, justify="center")
 
 
+    def display_article_summary(self,link: str):
+        #TODO Change headlines to whatever response is
+        # if headline in headlines:
+        #     self.console.print(f'[u]{Panel(get_summary(link))}', justify="center")
+        self.console.print(Panel(get_headlines()[0][1]), Panel(get_headlines()[0][1]), justify="center")
+        self.console.print(f'Input (r)eturn to return to the main menu or type (q) to quit', justify="center")
 
+
+
+# inspect('', methods=True)
+# inspect(Panel, methods=True)
 menu = Menu()
+menu.display_article_summary('Hello')
 # menu.ascii_art()
 # # menu.progress_bar()
-# menu.headlines_table()
+# menu.make_headlines_table()
 # menu.categories_panel()
 # menu.query_user()
 # menu.get_user_input()
-menu.display_category_news('Business')
+# menu.display_category_news('Business')
