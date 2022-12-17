1de2ffdc78cf2ecffa3efd65f0f44b0ab4415fc4
Daniel Brott <danbrott42@gmail.com>
2022-12-17 21:28:51 +0000

    added some further functionality

--- news/menu.py
+++ news/menu.py
@@ -10,7 +10,9 @@ import time
 from rich import inspect
 from rich.columns import Columns
 from rich.table import Column
+from rich.prompt import Prompt, Confirm
 import pyfiglet
+import sys
 from rich import box
 
 
@@ -22,16 +24,12 @@ class Menu:
 
 
     def ascii_art(self):
-        self.console.print(pyfiglet.figlet_format("WELCOME TO NEWS BYTES", font="slant", width=200), justify="center")
+        self.console.print(pyfiglet.figlet_format("WELCOME TO\n NEWS BYTES", font="slant", width=200), justify="center")
 
     @staticmethod
     def progress_bar(lst:list):
-        text_column = TextColumn("{task.description}", table_column=Column(ratio=1))
-        bar_column = BarColumn(bar_width=None, table_column=Column(ratio=2))
-        progress = Progress(text_column, bar_column, expand=True)
-        with progress:
-            for _ in progress.track(lst):
-                time.sleep(0.1)
+        for _ in track(lst, description="[green], fetching articles"):
+            time.sleep(0.02)
 
 
     def make_headlines_table(self, headlines:list[str] = get_headlines()):
@@ -44,6 +42,12 @@ class Menu:
             table.add_row(str(index + 1),headline[0])
         self.console.print(table, justify="center")
 
+    @staticmethod
+    def check_index(number:num, numbers_list:list[num]):
+        if number in numbers_list:
+            return numbers_list.index(number)
+        return False
+
     def categories_panel(self):
         news_list:list[str] = ['[magenta]Business', '[blue]World News', '[magenta]Tech', '[blue]Sports']
         categories = [Panel(category, expand=True, box=box.HEAVY_EDGE) for category in news_list]
@@ -52,19 +56,18 @@ class Menu:
     def query_user(self):
         self.console.print(Panel('Enter a [red]category [white]if you wish to see more headlines,\n otherwise enter in a headline number in order to get a summary \n with a link to the article. If you wish to quit, enter (q)uit'), justify="center", style='Bold')
 
-    def get_user_input(self):
-        news = ['business', 'world news', 'tech', 'sports']
-        user_input = input('> ')
-        while user_input.lower() != 'q' or user_input.lower() != 'quit':
-            if user_input in self.get_headlines().lower():
-                #TODO: When the user specifies a headline we return that specific article
+    @staticmethod
+    def get_user_input():
+        while True:
+            user_input = Prompt.ask("Select a category", choices=['Business', 'World News', 'Tech', 'Sports'])
+            if user_input.lower() == 'q' or user_input.lower() == 'quit':
+                if Confirm.ask("Are you sure you would like to quit?"):
+                    sys.exit()
+            elif user_input in temp_list:
+                #TODO display category headlines or text
                 pass
-            elif user_input.lower() in news:
-                #TODO: When the user specifies a category we return a new list of articles in that category
-                pass
-            elif user_input.lower() == 'r' or user_input.lower() == 'return':
-                #TODO Returns user to a previous menu or state
-                continue
+
+
 
     def display_category_news(self, category:str, headlines: list[str] = get_headlines()):
         table = Table(show_lines=True, row_styles=["cyan", "magenta"], title_justify="center", box=box.HEAVY_EDGE, highlight=True)
@@ -78,10 +81,10 @@ class Menu:
         self.console.print(table, justify="center")
 
 
-    def display_article_summary(self,link: str):
+    def display_article_summary(self,headline: tuple = get_headlines()):
         #TODO Change headlines to whatever response is
-        # if headline in headlines:
-        #     self.console.print(f'[u]{Panel(get_summary(link))}', justify="center")
+        # if user_input in headlines:
+        #     self.console.print(f'[u]{Panel(get_summary(headline[0][1]))}', justify="center")
         self.console.print(Panel(get_headlines()[0][1]), Panel(get_headlines()[0][1]), justify="center")
         self.console.print(f'Input (r)eturn to return to the main menu or type (q) to quit', justify="center")
 
