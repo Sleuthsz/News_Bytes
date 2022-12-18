9c95e34dd02bbec0364c5e863c243820a84be936
Natalija Germek <n.germek@icloud.com>
2022-12-17 17:40:48 -0800

    scraping world news, completed refactor to business news category

--- news/nbc.py
+++ news/nbc.py
@@ -13,12 +13,12 @@ class NBC:
     load_dotenv()
     homedir = os.path.expanduser("~")
     webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
-    nbc_driver = webdriver.Chrome(options=options, service=webdriver_service)
-    nbc_driver.get("https://www.nbcnews.com")
-    nbc_driver.implicitly_wait(1)
+    driver = webdriver.Chrome(options=options, service=webdriver_service)
+    driver.get("https://www.nbcnews.com")
+    driver.implicitly_wait(1)
 
     def get_news_headlines(self):
-        link = self.nbc_driver.find_elements(By.CLASS_NAME, "related-content__headline-link")
+        link = self.driver.find_elements(By.CLASS_NAME, "related-content__headline-link")
 
         headlines_links = []
         for i in range(len(link)):
@@ -28,8 +28,8 @@ class NBC:
         return headlines_links
 
     def get_article_text(self, link):
-        self.nbc_driver.get(link)
-        article = self.nbc_driver.find_element(By.TAG_NAME, "article")
+        self.driver.get(link)
+        article = self.driver.find_element(By.TAG_NAME, "article")
         elements = article.find_elements(By.XPATH, "//div[@data-component='text-block']")
         text = ""
         for element in elements:
@@ -37,7 +37,8 @@ class NBC:
         return text
 
     def get_business_headlines(self):
-        section = self.nbc_driver.find_elements(By.TAG_NAME, 'section')[2]
+        self.driver.get("https://www.nbcnews.com/business")
+        section = self.driver.find_elements(By.TAG_NAME, 'section')[2]
         business_links = section.find_elements(By.TAG_NAME, 'a')
 
         business_headlines_links = []
@@ -54,19 +55,19 @@ class NBC:
         return business_headlines_links[0:10]
 
     def get_world_news_headlines(self):
-        self.nbc_driver.get('https://www.nbcnews.com/world')
-        world_news_link = self.nbc_driver.find_elements(By.CLASS_NAME, "wide-tease-item__headline")
+        self.driver.get('https://www.nbcnews.com/world')
+        world_news_link = self.driver.find_elements(By.CLASS_NAME, "related-content__headline")
 
         world_news_headlines_links = []
-        for i in range(10):
+        for i in range(len(world_news_link)):
             world_news_headlines_links.append((world_news_link[i].text, world_news_link[i].get_attribute('href')))
         # print(link[i].get_attribute('href'))
         #     self.get_article_text()
         return world_news_headlines_links
 
     def get_tech_headlines(self):
-        self.nbc_driver.get('https://www.nbcnews.com/tech-media')
-        tech_news_link = self.nbc_driver.find_elements(By.CLASS_NAME, "wide-tease-item__headline")
+        self.driver.get('https://www.nbcnews.com/tech-media')
+        tech_news_link = self.driver.find_elements(By.CLASS_NAME, "wide-tease-item__headline")
 
         tech_news_headlines_links = []
         for i in range(10):
@@ -76,8 +77,8 @@ class NBC:
         return tech_news_headlines_links
 
     def get_sports_headlines(self):
-        self.nbc_driver.get('https://www.nbcsports.com/?cid=eref:nbcnews:text')
-        sports_link = self.nbc_driver.find_elements(By.CLASS_NAME, "more-headlines__list-item")
+        self.driver.get('https://www.nbcsports.com/?cid=eref:nbcnews:text')
+        sports_link = self.driver.find_elements(By.CLASS_NAME, "more-headlines__list-item")
 
         sports_links = []
         for i in range(len(sports_links)):
@@ -96,14 +97,17 @@ class NBC:
 
 if __name__ == "__main__":
     nbc = NBC()
+    # working
     headlines = nbc.get_news_headlines()
-    print(headlines)
-    links = nbc.get_article_text(headlines[1][1])
-    print(links)
+    # print(headlines)
+    # article = nbc.get_article_text(headlines[1][1])
+    # print(article)
     business = nbc.get_business_headlines()
-    print(business)
-    # tech = nbc.get_tech_headlines()
-    # print(tech)--broken
+    # print(business)
+    tech = nbc.get_tech_headlines()
+    # print(tech)
+    world = nbc.get_world_news_headlines()
+    print(world)
     # sports = nbc.get_sports_headlines()
     # print(sports)
     # article_test = get_article_text()
