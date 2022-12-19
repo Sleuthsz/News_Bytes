13e4395620a02c7483fc130bf5dce9cc31f65826
Andy Nguyen <a.d.nguyen.andy@gmail.com>
2022-12-18 19:41:32 -0800

    Get headlines from news categories.

--- news_scraper/menu.py
+++ news_scraper/menu.py
@@ -46,6 +46,97 @@ class Menu:
                     new_list += future.result()[:2]
         return new_list
 
+    def get_business_headlines(self):
+        business_headlines = []
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            for i in range(4):
+                if i == 0:
+                    future = executor.submit(self.bbc.get_business_headlines)
+                    business_headlines += future.result()[:2]
+                if i == 1:
+                    future = executor.submit(self.guardian.get_business_headlines)
+                    business_headlines += future.result()[:2]
+                if i == 2:
+                    future = executor.submit(self.cbs.get_business_headlines)
+                    business_headlines += future.result()[:2]
+                if i == 3:
+                    future = executor.submit(self.reuters.get_business_headlines)
+                    business_headlines += future.result()[:2]
+        return business_headlines
+
+    def get_world_news_headlines(self):
+        world_news_headlines = []
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            for i in range(4):
+                if i == 0:
+                    future = executor.submit(self.bbc.get_world_news_headlines)
+                    world_news_headlines += future.result()[:2]
+                if i == 1:
+                    future = executor.submit(self.guardian.get_world_news_headlines)
+                    world_news_headlines += future.result()[:2]
+                if i == 2:
+                    future = executor.submit(self.cbs.get_world_news_headlines)
+                    world_news_headlines += future.result()[:2]
+                if i == 3:
+                    future = executor.submit(self.reuters.get_world_news_headlines)
+                    world_news_headlines += future.result()[:2]
+        return world_news_headlines
+
+    def get_tech_headlines(self):
+        tech_headlines = []
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            for i in range(4):
+                if i == 0:
+                    future = executor.submit(self.bbc.get_tech_headlines)
+                    tech_headlines += future.result()[:2]
+                if i == 1:
+                    future = executor.submit(self.guardian.get_tech_headlines)
+                    tech_headlines += future.result()[:2]
+                if i == 2:
+                    future = executor.submit(self.cbs.get_tech_headlines)
+                    tech_headlines += future.result()[:2]
+                if i == 3:
+                    future = executor.submit(self.reuters.get_tech_headlines)
+                    tech_headlines += future.result()[:2]
+        return tech_headlines
+
+    def get_sport_headlines(self):
+        sports_headlines = []
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            for i in range(4):
+                if i == 0:
+                    future = executor.submit(self.bbc.get_sports_headlines)
+                    sports_headlines += future.result()[:2]
+                if i == 1:
+                    future = executor.submit(self.guardian.get_sports_headlines)
+                    sports_headlines += future.result()[:2]
+                if i == 2:
+                    future = executor.submit(self.cbs.get_sports_headlines)
+                    sports_headlines += future.result()[:2]
+                if i == 3:
+                    future = executor.submit(self.reuters.get_sports_headlines)
+                    sports_headlines += future.result()[:2]
+        return sports_headlines
+
+    def get_category_headlines(self, user_input, all_headlines):
+        match user_input.lower():
+            case "business":
+                if all_headlines["business"] is None:
+                    all_headlines["business"] = self.get_business_headlines()
+                self.display_category_news(all_headlines["business"])
+            case "world news":
+                if all_headlines["world_news"] is None:
+                    all_headlines["world_news"] = self.get_world_news_headlines()
+                self.display_category_news(all_headlines["world_news"])
+            case "tech":
+                if all_headlines["tech"] is None:
+                    all_headlines["tech"] = self.get_tech_headlines()
+                self.display_category_news(all_headlines["tech"])
+            case "sports":
+                if all_headlines["sports"] is None:
+                    all_headlines["sports"] = self.get_sport_headlines()
+                self.display_category_news(all_headlines["sports"])
+
     def ascii_art(self):
         self.console.print(pyfiglet.figlet_format("WELCOME TO\n NEWS BYTES", font="slant", width=200), justify="center")
 
@@ -79,11 +170,11 @@ class Menu:
         self.console.print(Panel('Enter a [red]category [white]if you wish to see more headlines,\n otherwise enter in a headline number in order to get a summary \n with a link to the article. If you wish to quit, enter (q)uit'), justify="center", style='Bold')
 
 
-    def display_category_news(self, category:str, headlines: list[str]):
+    def display_category_news(self, headlines: list[str]):
         table = Table(show_lines=True, row_styles=["cyan", "magenta"], title_justify="center", box=box.HEAVY_EDGE, highlight=True)
 
         table.add_column("#", justify="center")
-        table.add_column(f'{category} news_scraper', justify="center")
+        table.add_column('news_scraper', justify="center")
 
         for index, headline in enumerate(headlines):
             table.add_row(str(index + 1), headline[0])
@@ -121,7 +212,14 @@ class Menu:
         self.console.print(f'Input (r)eturn to return to the main menu or type (q) to quit', justify="center")
 
     def get_user_input(self):
+        categories = ["business", "world news", "tech", "sports"]
         headlines = self.get_headlines()
+        all_headlines = {
+            "business": None,
+            "world_news": None,
+            "tech": None,
+            "sports": None
+        }
         while True:
             self.make_headlines_table()
             self.categories_panel()
@@ -133,17 +231,18 @@ class Menu:
                 continue
             elif user_input.lower() == 'r' or user_input.lower() == 'return':
                 continue
-            for i, headline in enumerate(headlines):
-                if i == int(user_input):
-                    self.display_article_summary(headline[1])
-                print("Invalid input")
+            elif user_input.lower() in categories:
+                self.get_category_headlines(user_input.lower(), all_headlines)
+            elif user_input.isnumeric():
+                for i, headline in enumerate(headlines):
+                    if i == int(user_input):
+                        self.display_article_summary(headline[1])
+                    print("Invalid input")
 
 
     def main(self):
         self.ascii_art()
         self.get_user_input()
-
-
 
 
 
