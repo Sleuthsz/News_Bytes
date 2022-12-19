3c85d342fbdb7ffc5bb64a2e505341cccca01d6d
Natalija Germek <n.germek@icloud.com>
2022-12-18 23:49:54 -0800

    refactored class, refactored methods, troubleshoot methods and continued scrape.

--- news/nbc.py
+++ news/nbc.py
@@ -36,6 +36,32 @@ class NBC:
             text += element.text
         return text
 
+    def get_category_headlines(self, link):
+        self.driver.get(link)
+        self.driver.implicitly_wait(1)
+        section = self.driver.find_elements(By.TAG_NAME, 'section')[2]
+        links = section.find_elements(By.TAG_NAME, 'a')
+
+        headlines_links = []
+        for element in links:
+
+            link = element.get_attribute('href')
+            try:
+                head = element.find_element(By.TAG_NAME, 'h2').text
+            except:
+                headlines_links.append((head, link))
+                continue
+        return headlines_links[0:10]
+    #
+    # def get_business_headlines(self):
+    #     return self.get_category_headlines("https://www.nbcnews.com/business")
+    #
+    # def get_world_news_headlines(self):
+    #     return self.get_category_headlines("https://www.nbcnews.com/world")
+    #
+    # def get_tech_headlines(self):
+    #     return self.get_category_headlines("https://www.nbcnews.com/tech-media")
+
     def get_business_headlines(self):
         self.driver.get("https://www.nbcnews.com/business")
         section = self.driver.find_elements(By.TAG_NAME, 'section')[2]
@@ -43,20 +69,17 @@ class NBC:
 
         business_headlines_links = []
         for element in business_links:
-
             link = element.get_attribute('href')
             try:
                 head = element.find_element(By.TAG_NAME, 'h2').text
             except:
                 continue
-
             business_headlines_links.append((head, link))
-
         return business_headlines_links[0:10]
 
     def get_world_news_headlines(self):
         self.driver.get('https://www.nbcnews.com/world')
-        world_news_link = self.driver.find_elements(By.CLASS_NAME, "related-content__headline")
+        world_news_link = self.driver.find_elements(By.CLASS_NAME, "related-content__headline-link")
 
         world_news_headlines_links = []
         for i in range(len(world_news_link)):
@@ -70,21 +93,23 @@ class NBC:
         tech_news_link = self.driver.find_elements(By.CLASS_NAME, "wide-tease-item__headline")
 
         tech_news_headlines_links = []
-        for i in range(10):
-            tech_news_headlines_links.append((tech_news_link[i].text, tech_news_link[i].get_attribute('href')))
+        for i in range(len(tech_news_link)):
+            tech_news_headlines_links.append((tech_news_link[i].text, tech_news_link[i].value_of_css_property('data-activity-map=lead-package-link-2')))
         # print(link[i].get_attribute('href'))
         #     self.get_article_text()
         return tech_news_headlines_links
 
     def get_sports_headlines(self):
-        self.driver.get('https://www.nbcsports.com/?cid=eref:nbcnews:text')
-        sports_link = self.driver.find_elements(By.CLASS_NAME, "more-headlines__list-item")
+        self.driver.get('https://www.nbcsports.com/')
+        self.driver.implicitly_wait(1)
 
-        sports_links = []
-        for i in range(len(sports_links)):
-            sports_links.append((sports_link[i].text, sports_link[i].get_attribute('href')))
-        # print(link[i].get_attribute('href'))
-        return sports_link
+        lis = self.driver.find_elements(By.XPATH, '//aside/div/div[2]/div/div/ul/li')
+
+        headlines_links = []
+        for li in lis:
+            a = li.find_element(By.TAG_NAME, 'a')
+            headlines_links.append((a.text, a.get_attribute('href')))
+        return headlines_links
 
 
 # nbc_key = os.getenv("NBC_KEY")
@@ -93,27 +118,32 @@ class NBC:
 # ********* README.md update - created nbc.py for webscrape of nbc website, updated README.md - December 14, 2022
 # ********* README.md update - finalized headline and link scrape, started categories scrape, updated README.md. -
 # December 15, 2022
+# ********* README.md update - created NBC class and refactored to methods. Troubleshooting methods working properly,
+# updated README.md. - December 17, 2022
+# ********* README.md update - refactored NBC class and refactored to methods. Troubleshooting methods with team pivot
+# of method names and organization, updated README.md. - December 18, 2022
 
 
 if __name__ == "__main__":
     nbc = NBC()
     # working
     headlines = nbc.get_news_headlines()
-    # print(headlines)
-    # article = nbc.get_article_text(headlines[1][1])
-    # print(article)
+    print(headlines)
     business = nbc.get_business_headlines()
-    # print(business)
-    tech = nbc.get_tech_headlines()
-    # print(tech)
+    print(business)
     world = nbc.get_world_news_headlines()
     print(world)
-    # sports = nbc.get_sports_headlines()
-    # print(sports)
-    # article_test = get_article_text()
-    # print(article_test)
-    # categories = get_categories()
-    # print()
+    sports = nbc.get_sports_headlines()
+    print(sports)
+
+    # not working
+
+    categories = nbc.get_category_headlines()
+    print(categories)
+
+    # almost working
+    tech = nbc.get_tech_headlines()
+    print(tech)
 
     # ------------------unused until able to get rest working-------------
     # article_text = get_article_text(links[1][1])
@@ -122,7 +152,7 @@ if __name__ == "__main__":
     # print(article_text)
     # get_article_text()
 
-# --------
+# -------- code for ChatGPT
 
 # homedir = os.path.expanduser("~")
 # webdriver_service = Service(f"{homedir}/chromedriver/stable/chromedriver")
@@ -144,5 +174,4 @@ if __name__ == "__main__":
 #         presence_penalty=1
 #     )
 #     return response["choices"][0]["text"]
-#
 #
